130b9faa997eeb1b43660926648650819138b835
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-05-19 12:53:49 -0400

    Integration Test Feature 1 Scenario 1

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,11 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import { getEvents } from '../api';
 import EventList from '../components/EventList';
-import mockData from '../mock-data';
+import App from "../App";
+//import mockData from '../mock-data';
 
-jest.mock('../api', () => ({
+/*jest.mock('../api', () => ({
   getEvents: jest.fn()
-}));
+}));*/
 
 describe('<EventList /> component', () => {
     let EventListComponent;
@@ -18,10 +19,22 @@ describe('<EventList /> component', () => {
   });
 
   test('renders correct number of events', async () => {
-    getEvents.mockResolvedValue(mockData);
+//    getEvents.mockResolvedValue(mockData);
 
     const allEvents = await getEvents(); 
     EventListComponent.rerender(<EventList events={allEvents} />);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+  });
+});
+
+describe('<EventList /> integration', () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
