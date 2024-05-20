bca99965e3e6a6227bc114445d315f6541e82331
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-05-20 12:13:33 -0400

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,7 +1,7 @@
 import { render, within, waitFor } from '@testing-library/react';
 import { getEvents } from '../api';
 import EventList from '../components/EventList';
-import App from "../App";
+import App from '../App';
 //import mockData from '../mock-data';
 
 /*jest.mock('../api', () => ({
@@ -31,9 +31,9 @@ describe('<EventList /> integration', () => {
   test('renders a list of 32 events when the app is mounted and rendered', async () => {
     const AppComponent = render(<App />);
     const AppDOM = AppComponent.container.firstChild;
-    const EventListDOM = AppDOM.querySelector('#event-list');
+    const EventListDOM = AppDOM.querySelector("#event-list");
     await waitFor(() => {
-      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
       expect(EventListItems.length).toBe(32);
     });
   });
