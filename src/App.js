9582f96d7378ff0b96926cc377676c16374951dc
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-05-24 10:58:51 -0400

    Styling Updates
    -Trying to move NOE filed under the CitySearch field.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import CitySearch from './components/CitySearch';
-import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
@@ -28,8 +28,8 @@ const App = () => {
   return (
     <div className="App">
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <EventList events={events} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
+      <EventList events={events} />
     </div>
   );
 }
